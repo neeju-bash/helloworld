a0418bede81810ba20fe1bf127a29aaf01560ee9
MC <neeraj.mohan@capgemini.com>
2022-03-14 14:31:31 +0530

    spring boot

---
+++ src/main/java/com/cg/bootdemo/helloworld/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.cg.bootdemo.helloworld;
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+public class HelloWorldController   
+{  
+	@RequestMapping("/hello")  
+	public String hello()   
+	{  
+		return "Hello alll";  
+	}  
+}  
